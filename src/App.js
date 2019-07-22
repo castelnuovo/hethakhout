ad6fd12b5e8fac53e2e3bb117985709d6e70536f
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-07-22 09:57:31 +0200

    added preloader removal function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Het Hak Hout</h1>
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        // Remove preloader
+        const preloader = document.querySelector('#preloader');
+        preloader.classList.add('loaded');
+        setTimeout(() => {
+            preloader.outerHTML = '';
+        }, 100);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Het Hak Hout</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
