daf3b0798451f4e358ad8ba3c5016b56c74c514b
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-08-11 08:37:10 -0700

    updated navbar logic

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -42,6 +42,7 @@ class Navbar extends Component {
         this.setState({
             homePage: location.pathname === '/',
             scrolledPastBreakpoint: location.pathname !== '/',
+            hamburgerOpen: false,
             hamburgerPrevState: location.pathname !== '/'
         });
     }
