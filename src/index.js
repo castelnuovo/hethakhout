f705a1b76be5d328b27d038a2c7d8eadf2bdc511
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-10-12 14:26:07 +0200

    removed service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'Components/App';
-import { unregister } from 'register-service-worker';
-
-// import 'bulma/css/bulma.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-unregister();
