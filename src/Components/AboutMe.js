dea07329530e4b309838f2a7e2e9cd38c0a56890
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-11-06 16:12:52 +0100

    FIx #8 and removed useless text from homepage

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Profile_Picture from 'Assets/images/francisca_laporte.jpg';
+
+const AboutMe = () => {
+    return (
+        <section id="ciskaComponent" className="section">
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-one-third">
+                        <figure className="image">
+                            <img
+                                className="is-rounded"
+                                src={Profile_Picture}
+                                alt="Het hak hout: Francisca Laporte"
+                            />
+                        </figure>
+                    </div>
+                    <div className="column is-two-thirds">
+                        <h1 className="title">Francisca Laporte</h1>
+                        <p className="subtitle">
+                            Hippe slogan of zo? (misschien iets met
+                            shinrin-yoku)
+                        </p>
+                        <div className="content">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Repellat omnis quidem debitis, tenetur atque
+                            iusto exercitationem, libero ab aperiam porro
+                            inventore nihil corrupti, ullam alias harum
+                            excepturi neque sit eos?
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMe;
