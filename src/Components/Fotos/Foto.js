be1fb33e591d801dd8e8b25d1256ee2828b569c5
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-12-16 13:26:33 +0100

    added foto system

--- src/Components/Fotos/Foto.js
+++ src/Components/Fotos/Foto.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Foto = ({ description, url }) => {
+const Foto = ({ data }) => {
+    const thumbnail_url = data.foto.data.thumbnails[2].url;
+    const full_url = data.foto.data.full_url;
+    const [modalOpen, setModalOpen] = useState(false);
+
     return (
-        <div class="column is-one-third-desktop is-half-tablet">
-            <div class="card">
-                <div class="card-image">
-                    <figure class="image is-3by2">
-                        <img src={url} alt={description} />
-                    </figure>
+        <>
+            <div
+                className="column is-one-third-desktop is-half-tablet"
+                onClick={() => setModalOpen(true)}
+            >
+                <div className="card">
+                    <div className="card-image">
+                        <figure className="image is-3by2">
+                            <img src={thumbnail_url} alt={data.description} />
+                        </figure>
+                    </div>
+                    <footer className="card-footer">
+                        <p className="card-footer-item">{data.description}</p>
+                    </footer>
                 </div>
-                <footer class="card-footer">
-                    <p class="card-footer-item">{description}</p>
-                </footer>
             </div>
-        </div>
+            {modalOpen && (
+                <div className="modal is-active">
+                    <div className="modal-background"></div>
+                    <div className="modal-content">
+                        <p className="image is-4by3">
+                            <img src={full_url} alt={data.description} />
+                        </p>
+                    </div>
+                    <button
+                        className="modal-close is-large"
+                        aria-label="close"
+                        onClick={() => setModalOpen(false)}
+                    ></button>
+                </div>
+            )}
+        </>
     );
 };
 
--- src/Components/Fotos/Fotos.js
+++ src/Components/Fotos/Fotos.js
@@ -3,41 +3,21 @@ import useLocalStorage from 'Utils/useLocalStorage';
 import Loader from 'Components/Loader';
 import Foto from './Foto';
 
-// TODO: pull fotos from local storage
-
 const Fotos = () => {
     const [data] = useLocalStorage('fotoData');
 
-    if (!data) {
-        return <Loader />;
-    }
-
     return (
-        <div class="columns is-multiline">
-            <Foto
-                url="https://unsplash.it/300/200/?random&pic=1"
-                description="Description 1"
-            />
-            <Foto
-                url="https://unsplash.it/300/200/?random&pic=2"
-                description="Description 2"
-            />
-            <Foto
-                url="https://unsplash.it/300/200/?random&pic=3"
-                description="Description 3"
-            />
-            <Foto
-                url="https://unsplash.it/300/200/?random&pic=4"
-                description="Description 4"
-            />
-            <Foto
-                url="https://unsplash.it/300/200/?random&pic=5"
-                description="Description 5"
-            />
-            <Foto
-                url="https://unsplash.it/300/200/?random&pic=6"
-                description="Description 6"
-            />
+        <div className="columns is-multiline">
+            {!data && <Loader />}
+            {data &&
+                data.map(foto => (
+                    <Foto
+                        key={foto.id}
+                        data={foto}
+                        url={`https://unsplash.it/300/200/?random&pic=${foto.foto}`}
+                        description={foto.description}
+                    />
+                ))}
         </div>
     );
 };
