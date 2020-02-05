19efb751a4441b4dc7ff4df83da98261c4407c5b
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-02-05 15:02:33 +0100

    green loader, removed old dependency

--- src/Config/Brand.js
+++ src/Config/Brand.js
@@ -3,10 +3,11 @@ import orange_loader from 'Assets/videos/loader_orange.gif';
 import orange_hero from 'Assets/videos/hero_orange.gif';
 
 import green_logo from 'Assets/images/logo_green.png';
+import green_loader from 'Assets/images/loader_green.png';
 
 const Month = new Date().getMonth() + 1;
 const isOrange = Month < 4 || Month > 8;
 
 export const Hero = isOrange ? orange_hero : orange_hero; // TODO: green hero
 export const Logo = isOrange ? orange_logo : green_logo;
-export const Loader = isOrange ? orange_loader : orange_loader; // TODO: green loader
+export const Loader = isOrange ? orange_loader : green_loader;
