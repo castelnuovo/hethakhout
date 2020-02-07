2f50b7a500a2a87aab275b7fac1f17af40307f15
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-02-07 12:19:25 +0100

    fix wrong pathname

--- src/Config/Brand.js
+++ src/Config/Brand.js
@@ -3,7 +3,7 @@ import orange_loader from 'Assets/videos/loader_orange.gif';
 import orange_hero from 'Assets/videos/hero_orange.gif';
 
 import green_logo from 'Assets/images/logo_green.png';
-import green_loader from 'Assets/images/loader_green.png';
+import green_loader from 'Assets/videos/loader_green.gif';
 
 const Month = new Date().getMonth() + 1;
 const isOrange = Month < 4 || Month > 8;
