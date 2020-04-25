43ea515d9491997d1f3c804f5db238013219989e
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-04-25 12:22:19 +0200

    fix #118

---
+++ src/Config/Ackee.js
@@ -0,0 +1,12 @@
+const Ackee = {
+    server: {
+        server: 'https://analytics.lucacastelnuovo.nl',
+        domainId: 'd347ecdc-2348-4763-830e-beac8f498870',
+    },
+    options: {
+        ignoreLocalhost: true,
+        detailed: false,
+    },
+};
+
+export default Ackee;
